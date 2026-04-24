6fc63f7a3a474fd271c54b3361b5dd47ef2e91ae
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-04-24 08:57:32 +0545

    add i3lock and xautolock

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,9 @@ feh --randomize --bg-fill ~/Pictures/Walls/* &
 # Start picom (compositor)
 picom &
 
+# autolock 
+xautolock -time 10 -locker "i3lock -c 000000" &
+
 # Kill any existing polybar instances and start a new one
 ~/.config/polybar/launch.sh &
 
